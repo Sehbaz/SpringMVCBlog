9ca30aa5254a4dfb348858f8600a41706dae96a4
Sehbaz Rafik <sehbazrafik@MacBook-Pro.local>
2020-11-13 13:19:50 +0000

    Invoke the “PostService” from the Home Page Controller

--- src/main/java/blog/controllers/HomeController.java
+++ src/main/java/blog/controllers/HomeController.java
@@ -1,16 +1,28 @@
 package blog.controllers;
 
+import blog.models.Post;
+import blog.services.PostServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private PostServices postServices;
+
     @RequestMapping("/")
-    public String index(){
+    public String index(Model model){
+        //finding all the post in the postservices
+        List<Post> posts = postServices.findAll();
+
+        // transfer data to index.html file
+        model.addAttribute("posts", posts);
+
         return "index";
     }
-    public HomeController() {
-        System.out.println("*** HomeController ***");
-    }
-
 }
